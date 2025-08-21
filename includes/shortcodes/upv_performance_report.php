7c5bc3c565bea987509f195b70b69127c80111e1
John Anderson <info@soaringpenguin.com>
2025-08-20 19:46:18 -0700

    Add simple report to Performance Report page.

--- includes/shortcodes/upv_performance_report.php
+++ includes/shortcodes/upv_performance_report.php
@@ -28,54 +28,36 @@ function upv_performance_report()
     }
     ?>
     <p><a href="/performance-report/?performance_id=<?php echo $performance_id; ?>&download=true">Download CSV</a><p>
-    <!-- <table class="upv-table order-table">
+    <table class="upv-table order-table">
         <thead>
             <tr>
                 <td>Name</td>
                 <td>Paid status</td>
-                <td>Season</td>
-                <td>Preview</td>
-                <td>Student</td>
-                <td>Senior</td>
-                <td>Adult</td>
-                <td>Comp</td>
-                <td>Order Note</td>
-                <td>Order Note (Admin)</td>
-                <td>Customer Note (Admin)</td>
+                <td>Ticket Qty</td>
+                <td>Notes</td>
             </tr>
         </thead>
         <tbody>
             <?php
                 foreach( $tickets_sold as $order_id => $tickets )
-                {
+                { 
                     $order          = new WC_Order( $order_id ); 
-                    $name           = $order->get_billing_last_name() . ', ' . $order->get_billing_first_name(); 
+                    $last_name      = $order->get_billing_last_name();
+                    $first_name     = $order->get_billing_first_name();
+                    $name           = empty( $last_name . $first_name ) ? $order_id : $last_name . ', ' . $first_name;
                     $status         = $order->get_status();
                     $note           = $order->get_customer_note();
                     ?>
                     <tr>
-                        <td><?php echo $name; ?></td>
+                        <td><a href="/wp-admin/admin.php?page=wc-orders&action=edit&id=<?php echo $order_id; ?>" target="_blank"><?php echo $name; ?></a></td>
                         <td><?php echo $statuses[$status]; ?></td>
-                        <?php
-                            foreach( $ticket_types as $ticket_type_id => $ticket_name )
-                            {
-                                echo '<td>';
-                                if( array_key_exists( $ticket_type_id, $tickets ) )
-                                {
-                                    echo $tickets[$ticket_type_id]; 
-                                } else {
-                                    echo "&nbsp;";
-                                }
-                                echo '</td>';
-                            }
-
-                        ?>
+                        <td><?php echo array_sum($tickets); ?></td>
                         <td><?php echo $note; ?></td>
                     </tr>
                     <?php
                 }
             ?>
         </tbody>
-    </table> -->
+    </table>
     <?php
 }

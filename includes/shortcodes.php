a51c87a926068e88ae65f8948e284305b86e6c1f
spenguin <inof@soaringpenguin.com>
2025-09-25 22:34:30 -0700

    Create Performance shortcode.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -16,6 +16,7 @@ require_once CORE_SHORTCODE . 'upv_artistic_director.php';
 require_once CORE_SHORTCODE . 'upv_member_display.php';
 require_once CORE_SHORTCODE . 'upv_show_images.php';
 require_once CORE_SHORTCODE . 'upv_square_reconsciliation.php';
+require_once CORE_SHORTCODE . 'upv_reset_performance_titles.php';
 
 
 \Shortcodes\initialize();
@@ -36,4 +37,5 @@ function initialize()
     add_shortcode( 'upv_member_display', '\upv_member_display' );
     add_shortcode( 'upv_show_images', '\upv_show_images' );
     add_shortcode( 'upv_square_reconsciliation', '\upv_square_reconsciliation' );
+    add_shortcode( 'upv_reset_performance_titles', '\upv_reset_performance_titles' );
 }
---
+++ includes/shortcodes/upv_reset_performance_titles.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Render Audition parts
+ * (Might need to be extended or rebuilt if section is used elsewhere)
+ */
+
+function upv_reset_performance_titles( $atts = [], $content = null, $tag = '' )
+{
+    return '<p>Reset</p>';
+} 
